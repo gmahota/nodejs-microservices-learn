bb5dcee28e41a8e025c66640d4d6182bfc79ee70
Guimarães Mahota <guimaraesmahota@gmail.com>
2021-11-14 22:23:32 +0200

    another test git

--- ticketing/orders/src/routes/__test__/show.test.ts
+++ ticketing/orders/src/routes/__test__/show.test.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../../app';
 import { Ticket } from '../../models/ticket';
+import jwt from 'jsonwebtoken';
 
 let user:any 
 
